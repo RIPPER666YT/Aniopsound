f05eceeeea6bab927da1869540fb53aa6731a5eb
RIPPER666YT <rosomaha6567@yandex.ru>
2025-09-29 11:50:41 +0700

    Update opsound.js

--- opsound.js
+++ opsound.js
@@ -1,15 +1,16 @@
 const soundsData = [
     {
         id: "1",
-        title: "эта фарфоровая кукла влюбилась4534535534534535354354353535435353545",
+        title: "эта фарфоровая кукла влюбилась",
         file: "эта фарфоровая кукла влюбилась",
         image: "https://shikimori.one/uploads/poster/animes/48736/536021070760d16f823b485bc5b021a6.jpeg"
     },
     {
-        id: "2",
+        id: "4",
         title: "эта фарфоровая кукла влюбилась 2",
         file: "эта фарфоровая кукла влюбилась 2",
         image: "https://shikimori.one/uploads/poster/animes/53065/3f2a736b3aa5edf34fa93ff7e6d468c1.jpeg"
     }
 
 ];
+
